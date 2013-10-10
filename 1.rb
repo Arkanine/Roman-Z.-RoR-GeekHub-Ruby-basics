4bc4b2471870c227bb77b71e9cb646b0db6c2c01
Arkanine <zabila.r@ya.ru>
2013-10-11 00:49:03 +0300

    Create 1.rb

---
+++ 1.rb
@@ -0,0 +1,15 @@
+sec = 1000000000
+
+year = sec /60/60/24/365.to_f
+year_v = year/4
+
+second = sec - year_v*366*24*60*60
+
+month_n = second/60/60/24/365 * 12.to_f
+month = month_n % 12
+
+days_n = second/60/60/24 % 365
+mid = 365/12.to_f
+day = days_n % mid
+
+puts 'Age of the person is: ' + year.to_i.to_s + ' years, ' + month.to_i.to_s  + ' months and ' + day.to_i.to_s   + ' days.'
