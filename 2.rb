1eee6f99b6c675d0eaa6e3a4ecb100859229beb1
Arkanine <zabila.r@ya.ru>
2013-10-11 00:50:19 +0300

    Create 2.rb

---
+++ 2.rb
@@ -0,0 +1,9 @@
+
+puts 'Input your surname please!'
+surname = gets.chomp.capitalize
+puts 'Input your name please!'
+name = gets.chomp.capitalize
+puts 'Input your second name please!'
+second_name = gets.chomp.capitalize
+
+puts 'Hello mr. ' + surname + ' ' + name + ' ' + second_name + '.'
