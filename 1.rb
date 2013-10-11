8c00b9fe3f1a38cbc0dc9a10c235ba0742ce30df
Arkanine <zabila.r@ya.ru>
2013-10-11 11:27:04 +0300

    Update 1.rb

--- 1.rb
+++ 1.rb
@@ -1,15 +1,15 @@
 sec = 1000000000
 
-year = sec/60/60/24/365.to_f
-year_v = year/4
+year = sec/60/60/24/365.to_f                # всего год
+year_v = year/4                             # високосных
+        
+second = sec - year_v*366*24*60*60          # сколько секунд в НЕвисокосном году
 
-second = sec - year_v*366*24*60*60
+month_n = second/60/60/24/365 * 12.to_f     # сколько месяцев всего 
+month = month_n % 12                        # остача
 
-month_n = second/60/60/24/365 * 12.to_f
-month = month_n % 12
-
-days_n = second/60/60/24 % 365
-mid = 365/12.to_f
-day = days_n % mid
+days_n = second/60/60/24 % 365              # дней всего
+mid = 365/12.to_f                           # среднее арифметическое месяцев
+day = days_n % mid                          # остача
 
 puts 'Age of the person is: ' + year.to_i.to_s + ' years, ' + month.to_i.to_s  + ' months and ' + day.to_i.to_s   + ' days.'
