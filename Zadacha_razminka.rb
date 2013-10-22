c564cf506c8ad580111d9cf13c6f24ccc7771ac2
Arkanine <zabila.r@ya.ru>
2013-10-22 09:59:55 +0300

    Update Zadacha_razminka.rb

--- Zadacha_razminka.rb
+++ Zadacha_razminka.rb
@@ -1,17 +1,18 @@
-number  = (1.. 19).to_a
-
-number = 1
+number = 0
 power = 0
+
 while number!= 19
 	number += 1
-	if number % 2 == 0 and number <= 5
+	if number % 2 != 0 and number <= 4
 		power += 62
-		puts 'Generator #' +(number - 1).to_s + ' is off.'
-		puts 'Generator #' +number.to_s + ' is on, adding 62MW, for a total of ' + power.to_s + 'MW!'
-	elsif number % 2 == 0 and number >= 6
+		puts 'Generator #' + number.to_s + ' is off.'
+		puts 'Generator #' + (number + 1).to_s + ' is on, adding 62MW, for a total of ' + power.to_s + 'MW!'
+	elsif number % 2 != 0 and number >= 5
 		power += 124
-		puts 'Generator #' +(number - 1).to_s + ' is off.'
-		puts 'Generator #' +number.to_s + ' is on, adding 124MW, for a total of ' + power.to_s + 'MW!'
+		puts 'Generator #' + number.to_s + ' is off.'
+		puts 'Generator #' + (number + 1).to_s + ' is on, adding 124MW, for a total of ' + power.to_s + 'MW!'
+	elsif number == 18
+		puts 'Generator #19 is off.' 
+		exit
 	end
 end
-puts 'Generator #19 is off.'
